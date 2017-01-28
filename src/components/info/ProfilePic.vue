f13517c39aa46171b87fc71564acee848e08e7f9
Tercio de Melo <terciodemelo@gmail.com>
2017-01-28 19:13:35 -0300

    Enhances profile picture stylings

--- src/components/info/ProfilePic.vue
+++ src/components/info/ProfilePic.vue
@@ -11,4 +11,11 @@ export default {
 </script>
 
 <style scoped>
+img {
+  position: relative;
+  border: 3px solid white;
+  border-radius: 100%;
+  box-shadow: 0 0 0 1px lightgrey;
+  height: 200px;
+}
 </style>
