775e8634a919029e73c80fd6937a08b65fdc12de
Tercio de Melo <terciodemelo@gmail.com>
2017-01-28 19:24:01 -0300

    Styles info pieces in their own component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,36 +1,35 @@
 <template>
   <div class="home">
     <profile-pic></profile-pic>
-    <p>Tercio de Melo</p>
-    <p>email: terciodemelo@gmail.com</p>
 
-    <p>Twitter:
-      <a href="https://twitter.com/terciodemelo">
-        @terciodemelo
-      </a>
-    </p>
+    <info-piece :title="'Tercio de Melo'"></info-piece>
 
-    <p>GitHub: 
-      <a href="https://github.com/terciodemelo">
-        terciodemelo
-      </a>
-    </p>
+    <info-piece :title="'terciodemelo@gmail.com'"></info-piece>
 
-    <p>LinkedIn:
-      <a href="https://www.linkedin.com/in/tercio-de-melo-0b714010a/">
-        Tercio de Melo
-      </a>
-    </p>
+    <info-piece :title="'Twitter'"
+                :link="'https://twitter.com/terciodemelo'"
+                :content="'@terciodemelo'"></info-piece>
+              
+    <info-piece :title="'Github'"
+                :link="'https://github.com/terciodemelo'"
+                :content="'terciodemelo'"></info-piece>
+
+    <info-piece :title="'LinkedIn'"
+                :link="'https://www.linkedin.com/in/tercio-de-melo-0b714010a/'"
+                :content="'Tercio de Melo'"></info-piece>
+
   </div>
 </template>
 
 <script>
   import ProfilePic from './info/ProfilePic'
+  import InfoPiece from './info/InfoPiece'
 
   export default {
     name: 'home',
     components: {
-      ProfilePic
+      ProfilePic,
+      InfoPiece
     }
   }
 </script>
---
+++ src/components/info/InfoPiece.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="info-piece">
+    <p>
+      <span v-if="title">{{ title }}</span>
+      <a v-if="link" :href="link">
+        {{ content }}
+      </a>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'info-piece',
+    props: ['title', 'link', 'content']
+  }
+</script>
+
+<style scoped>
+</style>
