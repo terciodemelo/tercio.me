38c04618908591f29549fe5262c25f931d5d15d5
Tercio de Melo <terciodemelo@gmail.com>
2017-01-28 18:47:10 -0300

    Adds basic profile info

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="home">
+    <p><img src="../assets/tercio-profile.jpg" alt=""></p>
+    <p>Tercio de Melo</p>
+    <p>email: terciodemelo@gmail.com</p>
 
+    <p>Twitter:
+      <a href="https://twitter.com/terciodemelo">
+        @terciodemelo
+      </a>
+    </p>
+
+    <p>GitHub: 
+      <a href="https://github.com/terciodemelo">
+        terciodemelo
+      </a>
+    </p>
+
+    <p>LinkedIn:
+      <a href="https://www.linkedin.com/in/tercio-de-melo-0b714010a/">
+        Tercio de Melo
+      </a>
+    </p>
   </div>
 </template>
 
