06d2600911bf249d172edf0b8d1d35181c8de014
Tercio de Melo <terciodemelo@gmail.com>
2017-01-29 19:31:38 -0300

    Adds Experience component to project
    Each Experience component describes a job experience in the resumé

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,14 +1,88 @@
 <template>
   <div id="resume">
     <div class="box">
-      Resumé
+      <experience v-for="experience in experiences" 
+                  :title="experience.title"
+                  :where="experience.where"
+                  :period="experience.period"
+                  :description="experience.description"
+                  :technologies="experience.technologies">
+      </experience>
     </div>
   </div>
 </template>
 
 <script>
+import Experience from './resume/Experience'
+
 export default {
-  name: 'resume'
+  name: 'resume',
+  components: {
+    Experience
+  },
+  data () {
+    return {
+      experiences: [
+        {
+          title: `Full-Stack developer and Cloud Computing Engineer,
+            LSD-UFCG`,
+          where: 'Campina Grande - PB, Brazil',
+          period: 'Aug 2016 - Present',
+          description: `
+            Technical leader, in charge of integration between Lenovo LXCA
+            and RedHat ManageIQ; also responsible for the development of a
+            ruby client for LXCA`,
+          technologies: [
+            'Ruby on Rails', 'Docker', 'Postgres', 'MongoDB', 'ManageIQ',
+            'LXCA', 'Angular 2'
+          ]
+        },
+        {
+          title: 'Full-Stack developer and DevOps, Freelancer ',
+          where: 'Campina Grande - PB, Brazil',
+          period: 'Jul 2016 - Dec 2016',
+          description: `
+            Responsible for development and maintenance of web services
+            and SaaS projects, including containerization and building
+            continous deintegration and deployment pipelines`,
+          technologies: [
+            'Java', 'Jersey', 'Ruby', 'Ruby on Rails', 'Docker', 'Jenkins'
+          ]
+        },
+        {
+          title: 'Back-end Software Developer Intern, TFG Co',
+          where: 'São Paulo - SP, Brazil',
+          period: 'Feb 2015 - Jun 2016',
+          description: `
+            Responsible for building and enhancement of SaaS and web
+            services with heavy traffic requirements; also configuring
+            infrastructure to serve them with configuration managers in
+            HA environments with Kubernetes. Also responsible for
+            developing dev tools such as web scrapers, scripts, data
+            reporting and networking security mobile libraries, among
+            others`,
+          technologies: [
+            'Ruby', 'Ruby on Rails', 'Sinatra', 'Node.js', 'Python',
+            'Scala', 'Play! Framework', 'Java', 'Objective-C', 'Postgres',
+            'SQLite', 'DynamoDB', 'MongoDB', 'Redis', 'Kafka', 'Zookeeper',
+            'Chef', 'Ansible', 'Docker', 'Kubernetes', 'Sensu', 'EC2',
+            'DigitalOcean', 'OpsWorks'
+          ]
+        },
+        {
+          title: 'Software Developer, SPLab',
+          where: 'Campina Grande - PB, Brazil',
+          period: 'Feb 2014 - Dec 2014',
+          description: `
+            Responsible for building embedded systems for Ingenico
+            terminals, Android applications, and dev tools`,
+          technologies: [
+            'C', 'C++', 'Java', 'Android', 'Python'
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
---
+++ src/components/resume/Experience.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="experience">
+    <p>{{ title }}</p>
+    <p>{{ where }}</p>
+    <p>{{ period }}</p>
+    <p>{{ description }}</p>
+    <p>{{ technologies }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'experience',
+  props: ['title', 'where', 'period', 'description', 'technologies']
+}
+</script>
+
+<style scoped>
+</style>
